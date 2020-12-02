d6b7ce40799491bfa21dc1f9f10aaa41ae3a9272
siweiZhu <thinkzhu110929@gmail.com>
2020-12-02 21:37:41 +0800

    Completed Exercise #1.

--- HelloSwift/ViewController.swift
+++ HelloSwift/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to my first App!", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+       
+        present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
